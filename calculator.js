774f95044139e99e8b97486d7b0256843a39c749
Frederick <69136202+Fred-H-Coding@users.noreply.github.com>
2026-03-20 01:33:34 -0400

    Add button functionality. Display updates with button presses. Error message.

--- calculator.js
+++ calculator.js
@@ -1,6 +1,7 @@
-let a;
-let b;
-let operator;
+let a = "";
+let b = "";
+let operator = "";
+let solution;
 
 function add(a,b) {
     return a + b;
@@ -15,19 +16,74 @@ function multiply(a,b) {
 }
 
 function divide(a,b) {
-    let quotient = a/b;
-    let roundedQuotient = Number(quotient.toFixed(7));
-    return roundedQuotient;
+    if (b == 0 || b == "0") {
+        return numberDisplay.textContent = "You can't divide by zero!";
+    }
+    
+    else {
+        let quotient = a/b;
+        let roundedQuotient = Number(quotient.toFixed(7));
+        return roundedQuotient;
+    }
 }
 
-const numberButtons = document.querySelectorAll('div.numberButton');
+
+
+//Creates nodeLists and then arrays of number buttons and operation buttons //
+
+const numberButtons = document.querySelectorAll('button.numberButton');
 const numberButtonArray = Array.from(numberButtons);
 
+const operationButtons = document.querySelectorAll('button.operationButton');
+const operationButtonArray = Array.from(operationButtons);
 
-numberButtonArray.forEach((numberButton) => numberButton.addEventListener("click", () => a = this.textContent));
+const equalsignButton = document.querySelector('.equalsignButton');
+const clearButton = document.querySelector('.clearButton');
+
+//Event Listeners for Number Buttons. Pressing a number button concatenates string a.
+
+for (let i = 0; i<numberButtonArray.length; i++) {
+    numberButtonArray[i].addEventListener("click", () => {
+        if (operator === "") {
+            a += numberButtonArray[i].textContent;
+            numberDisplay.textContent = a;
+            return a; 
+        }
+        else {
+            b += numberButtonArray[i].textContent;
+            numberDisplay.textContent = b;
+            return b;
+        }
+    })
+}
+
+//Operator Button event Listeners added through mapping.
+
+operationButtonArray.map((opButton) => opButton.addEventListener("click", () => {
+    operator = opButton.textContent;
+    return operator;
+} ));
+
+equalsignButton.addEventListener("click", () => {
+    operate(a,operator,b);
+    numberDisplay.textContent = solution;
+} );
+
+clearButton.addEventListener("click", () => {
+    a = "";
+    b = "";
+    operator = "";
+    solution = 0;
+    numberDisplay.textContent = solution;
+});
+
+const numberDisplay = document.querySelector('.numberDisplay');
+
+// numberButtonArray.forEach((numberButton) => numberButton.addEventListener("click", () => a = this.textContent));
 
 function operate(a,operator,b) {
-    let solution;
+    a = Number(a);
+    b = Number(b);
     switch (operator) {
         case "+":
             solution = add(a,b);
@@ -35,10 +91,10 @@ function operate(a,operator,b) {
         case "-":
             solution = subtract(a,b);
             break;
-        case "*":
+        case "×":
             solution = multiply(a,b);
             break;
-        case "/":
+        case "÷":
             solution = divide(a,b);
             break;
     }
